61c869bb602679788f938fa112048a1d24f3e496
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-04 16:21:50 +0100

    complete code

--- src/main/java/Ecker_Fehler.java
+++ src/main/java/Ecker_Fehler.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Ecker_Fehler {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double relativerfehler;
+        double absuluterfehler;
+
+        System.out.print("Istwert");
+        double istwert = scanner.nextDouble();
+        System.out.print("Sollwert");
+        double sollwert = scanner.nextDouble();
+
+        absuluterfehler = sollwert - istwert;
+
+        if (absuluterfehler < 0) {
+            absuluterfehler = absuluterfehler * -1;
+        }
+
+        relativerfehler = 100 / sollwert * absuluterfehler;
+
+        System.out.println(absuluterfehler);
+        System.out.println(relativerfehler);
+
+
+
+
+    }
 }
